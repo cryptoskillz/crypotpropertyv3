14c21cc806b84b34f68cbbe24da184cd7c9c8836
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-11-28 16:10:34 +0700

    property processing

--- frontend/_data/api.js
+++ frontend/_data/api.js
@@ -1,5 +1,12 @@
 
+/*
 
+TODO
+
+rental agreements when we do more than 1.
+
+
+*/
 //remove the comments when you have wired up the API
 require('dotenv').config();
 const superagent = require('superagent');
@@ -9,8 +16,29 @@ getData = async () => {
   console.log(`${process.env.STRAPIAPI}property-details/`)
   var res = await superagent.get(`${process.env.STRAPIAPI}property-details/`).query({});
   //console.log(res.body)
-  return(res.body)
+
+
+
+  //property owners
+  console.log(`${process.env.STRAPIAPI}property-contracts/${res.body[0].rental_agreement.id}/`)
+  var res2 = await superagent.get(`${process.env.STRAPIAPI}property-contracts/${res.body[0].rental_agreement.id}/`).query({});
+  //console.log(res2.body)
+  res.body[0]._proptery_owners = res2.body.proptery_owners
+  //console.log(res.body)
+
+
+  //rental aggrement
+  console.log(`${process.env.STRAPIAPI}rental-agreements/${res.body[0].rental_agreement.id}/`)
+  var res3 = await superagent.get(`${process.env.STRAPIAPI}rental-agreements/${res.body[0].rental_agreement.id}/`).query({});
+  //console.log(res3.body)
+  res.body[0]._rental_agreements = res3.body
+  console.log(res.body[0]._rental_agreements)
   
+
+
+  return(res.body)
+ 
+
 }
 
 
