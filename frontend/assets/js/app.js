555583c38611078d4f7582667a7c2638c57f9b05
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-11-29 21:26:03 +0700

    property report

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,10 +1,11 @@
 //put your generic js functon here
-
 let whenDocumentReady = (f) => {
     /in/.test(document.readyState) ? setTimeout('whenDocumentReady(' + f + ')', 9) : f()
 }
-
 whenDocumentReady(isReady = () => {
-
-   
+    $('#rentaltable').DataTable();
+    $('#paidintable').DataTable();
+    $('#ownerstable').DataTable();
+    $('#paidouttable').DataTable();
+    $('#disttable').DataTable();
 })
